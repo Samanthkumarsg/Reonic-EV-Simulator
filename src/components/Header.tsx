eb68815aef0de84d94de0a583036ce5ed8b91247
samanth kumar <samanthkumarsg@gmail.com>
2024-12-16 04:11:32 +0100

    feature/image optimization

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
         <React.Fragment>
             <div className="w-full border-b py-4 flex items-center justify-between ">
                 <div className="px-6">
-                    <img src="/src/assets/reonic.svg" className="h-6 w-[74px]" alt="My Image" loading="lazy" />
+                    <img src="./src/assets/reonic.svg" className="h-6 w-[74px]" alt="My Image" loading="lazy" />
                 </div>
                 <div className="">
                     <button className="group bg-gray-950 hover:bg-gray-700 text-white rounded-full p-2 w-24 text-xs font-medium flex items-center justify-center flex-row">Read Me </button>
