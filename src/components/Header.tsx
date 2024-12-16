b19fe5fcc82d40f0132d106585965bf60d89a24a
samanth kumar <samanthkumarsg@gmail.com>
2024-12-16 05:06:42 +0100

    feature/Navigate

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,22 @@
-import * as React from "react"
+import React from "react"
 
 const Header: React.FC = () => {
     return (
-        <React.Fragment>
-            <div className="w-full border-b py-4 flex items-center justify-between ">
-                <div className="px-6">
-                    <img src="/reonic.svg" className="h-6 w-[74px]" alt="Reonic" />
-                </div>
-                <div className="">
-                    <button className="group bg-gray-950 hover:bg-gray-700 text-white rounded-full p-2 w-24 text-xs font-medium flex items-center justify-center flex-row">Read Me </button>
-                </div>
-
+        <div className="w-full border-b py-4 flex items-center justify-between ">
+            <div className="px-6">
+                <img src="/reonic.svg" className="h-6 w-[74px]" alt="Reonic" />
             </div>
-        </React.Fragment>
+            <div className="">
+                <a
+                    href="https://github.com/Samanthkumarsg/Reonic-EV-Simulator/tree/main#reonic-ev-simulator"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <button className="group bg-gray-950 hover:bg-gray-700 text-white rounded-full p-2 w-24 text-xs font-medium flex items-center justify-center flex-row">Read Me </button>
+                </a>
+            </div>
+
+        </div>
     )
 }
 
